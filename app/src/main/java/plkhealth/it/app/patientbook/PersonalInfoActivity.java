696da2170d2e39dae5b51470b04e29090d85ef68
tehnn <tehnnn@gmail.com>
2016-05-01 00:22:03 +0700

    Initial commit

---
+++ app/src/main/java/plkhealth/it/app/patientbook/PersonalInfoActivity.java
@@ -0,0 +1,41 @@
+package plkhealth.it.app.patientbook;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    SharedPreferences pref ;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+        toolbar = (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("ข้อมูลส่วนตัวครับ");
+
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String name = pref.getString("name", "");
+        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+
+}
