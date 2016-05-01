82ed27ce612d16648f6c195640c4b1541bf3609c
tehnn <tehnnn@gmail.com>
2016-05-01 10:59:22 +0700

    utehn phnu

--- app/src/main/java/plkhealth/it/app/patientbook/MainActivity.java
+++ app/src/main/java/plkhealth/it/app/patientbook/MainActivity.java
@@ -109,7 +109,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
+            Intent intent = new Intent(this,AdminLoginActivity.class);
+            startActivity(intent);
             return true;
         }
 
