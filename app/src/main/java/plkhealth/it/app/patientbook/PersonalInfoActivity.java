41a0ecef4e783a288a6b1f67a093e0b54514a179
tehnn <tehnnn@gmail.com>
2016-05-20 20:25:13 +0700

    utehn phnu

--- app/src/main/java/plkhealth/it/app/patientbook/PersonalInfoActivity.java
+++ app/src/main/java/plkhealth/it/app/patientbook/PersonalInfoActivity.java
@@ -1,20 +1,43 @@
 package plkhealth.it.app.patientbook;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
 
     MyGlobals myGlobal;
+    WebView webView ;
+
+    private void bindWidget(){
+        webView = (WebView) findViewById(R.id.wb_personal_info);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -25,7 +48,10 @@ public class PersonalInfoActivity extends AppCompatActivity {
         //pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         //String patient_cid = pref.getString("patient_cid", "");
         String patient_cid =myGlobal.getPatientCid();
-        Toast.makeText(getApplicationContext(),patient_cid,Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(),patient_cid,Toast.LENGTH_SHORT).show();
+
+        bindWidget();
+        webView.loadData(patient_cid, "text/html", "UTF-8");
     }
 
     @Override
@@ -34,6 +60,14 @@ public class PersonalInfoActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(menuItem);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 
 
