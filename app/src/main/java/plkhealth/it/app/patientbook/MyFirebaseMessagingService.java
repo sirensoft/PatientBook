82d2e0c9fcee04e1936f51201d1f6c785a2a5712
tehnn <tehnnn@gmail.com>
2016-05-27 00:06:07 +0700

    utehn phnu

--- app/src/main/java/plkhealth/it/app/patientbook/MyFirebaseMessagingService.java
+++ app/src/main/java/plkhealth/it/app/patientbook/MyFirebaseMessagingService.java
@@ -43,7 +43,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, DoctorLoginActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
