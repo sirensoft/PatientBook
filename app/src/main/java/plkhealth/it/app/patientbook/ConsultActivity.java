bffb27df225971e91f929bd016daa2a703e4519b
tehnn <tehnnn@gmail.com>
2016-06-24 00:15:42 +0700

    utehn phnu

--- app/src/main/java/plkhealth/it/app/patientbook/ConsultActivity.java
+++ app/src/main/java/plkhealth/it/app/patientbook/ConsultActivity.java
@@ -2,9 +2,64 @@ package plkhealth.it.app.patientbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.pixplicity.easyprefs.library.Prefs;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class ConsultActivity extends AppCompatActivity {
+
+    EditText txt_chat ;
+    //String chat;
+    public void add_data(final String chat){
+
+        final String url_input = Prefs.getString("api_url", "") + "frontend/web/chat/post";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = new FormBody.Builder()
+
+                        .add("patient_cid", Prefs.getString("patient_cid", ""))
+                        .add("chat_text",chat)
+                        .add("doctor_or_patient","patient")
+                        .add("hospcode","")
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(url_input)
+                        .post(body)
+                        .build();
+
+                try {
+                    client.newCall(request).execute();
+                    Log.d("chat post","ok");
+                    runOnUiThread(new Runnable(){
+                        @Override
+                        public void run(){
+                            // change UI elements here
+                            txt_chat.setText("");
+                        }
+                    });
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +68,17 @@ public class ConsultActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("ปรึกษาหมอ");
+        txt_chat = (EditText)findViewById(R.id.txt_chat);
     }
+    public void btn_post_chat_click(View view){
+        String mChat=txt_chat.getText().toString();
+        if(!mChat.trim().equals("")){
+            add_data(mChat);
+        }
+
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
         if (menuItem.getItemId() == android.R.id.home) {
