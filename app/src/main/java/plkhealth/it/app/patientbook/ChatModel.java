fe4a2897f4a44f869952e6fa16e80b5e30936561
tehnn <tehnnn@gmail.com>
2016-06-23 23:55:45 +0700

    utehn phnu

---
+++ app/src/main/java/plkhealth/it/app/patientbook/ChatModel.java
@@ -0,0 +1,14 @@
+package plkhealth.it.app.patientbook;
+
+/**
+ * Created by utehn on 23/6/2559.
+ */
+public class ChatModel {
+    public int id;
+    public String patient_cid;
+    public String chat_text;
+    public String doctor_or_patient;
+    public String hospcode;
+    public String send_date;
+    public String read;
+}
